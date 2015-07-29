b6e14d17b8ef824ec238272dd4fd014dcc871228
Kristian Kime <kristian.kime@gmail.com>
2015-07-29 18:30:51 -0400

    gradle project run on server now works

---
+++ gradle-importer/src/main/java/demo/GradleImporterApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleImporterApplication.class, args);
+    }
+}
